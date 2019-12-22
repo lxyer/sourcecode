f21b626f3280ffbc079c6eaf10e003c58fbe36c7
lxyer <lxyer1@gmail.com>
2019-12-22 08:48:32 +0800

    Initial commit

---
+++ src/main/java/com/lxyer/sourcecode/SourcecodeApplication.java
@@ -0,0 +1,13 @@
+package com.lxyer.sourcecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourcecodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SourcecodeApplication.class, args);
+    }
+
+}
